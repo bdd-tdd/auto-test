44450730fdeae8466011850777a4ddc559c0c7af
Lynn.Lin <Lynn.Lin@gd-ms.com>
2018-07-03 11:33:03 -0400

    set default url in serenity.properties; add config.properties

--- src/test/java/com/testdemo/todos/features/steps/RecordTodoSteps.java
+++ src/test/java/com/testdemo/todos/features/steps/RecordTodoSteps.java
@@ -10,7 +10,7 @@ public class RecordTodoSteps {
 	User user;
 	
 	@Given("^users can add an action \"([^\"]*)\" to the todo list$")
-	public void add_action_to_todo_list(String action) {
+	public void add_action_to_todo_list(String action) throws Throwable {
 		user.open_todo_application();
 		user.add_an_action_Called(action);
 		user.should_see_action_called(action);
